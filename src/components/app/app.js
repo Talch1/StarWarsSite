3e173af9f22b79650079e32bfe4e1a892aa66d91
Talch1 <geylert@gmail.com>
2021-03-13 13:11:40 +0200

    item List change

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,16 @@ import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list/";
 import PeopleDetails from "../people-details";
+import StarshipDetails from "../starship-details/starship-details";
 import "./app.css";
-import Spinner from "../spiner/spiner";
+import SwapiService from "../../services/swapi-service";
+import PlanetDetails from "../planets-details/planet-details";
+
 export default class App extends Component {
+  swapiService = new SwapiService();
   state = {
     seePlanet: true,
-    seeSpinerPerson :true,
+    seeSpinerPerson: true,
     selectedPerson: 5,
   };
 
@@ -23,10 +27,21 @@ export default class App extends Component {
   personOpen = (id) => {
     this.setState({
       selectedPerson: id,
-      seeSpinerPerson:false
+      selectedStarship: id,
+      seeSpinerPerson: false,
     });
   };
+
   render() {
+    const itemList = (
+      <ItemList
+        onItemSelected={this.personOpen}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, gender, birthYear }) =>
+          `${name} (${gender} ,${birthYear})`
+        }
+      />
+    );
     return (
       <div className="container">
         <Header />
@@ -37,11 +52,33 @@ export default class App extends Component {
           </button>
         </div>
         <div className="row">
+          <div className="col-5">{itemList}</div>
+          <div className="col-7 mt-4">
+            <PeopleDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+        <div className="row">
           <div className="col-5">
-            <ItemList onItemSelected={this.personOpen} />
+            <ItemList
+              onItemSelected={this.personOpen}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
+            />
           </div>
           <div className="col-7 mt-4">
-           <PeopleDetails personId={this.state.selectedPerson} />
+            <StarshipDetails /> personId={this.state.selectedStarship} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-5">
+            <ItemList
+              onItemSelected={this.personOpen}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name}
+            />
+          </div>
+          <div className="col-7 mt-4">
+            <PlanetDetails /> personId={this.state.selectedStarship} />
           </div>
         </div>
       </div>
